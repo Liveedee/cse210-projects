cb76ff2b8658f0b4f429e5b4fa1aaf18e3f2f93d
shaeloco <u360134758@byui.edu>
2023-01-06 02:17:01 -0500

    Hit discard changes Ctrl Z brought my work back

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,67 @@
-using System;
 
+// !!!!!!!!!!!! CORE REQUIREMENTS #1 AND #2 !!!!!!!!!!!!!!!!!!!!!!
+
+
+// using System;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.Write("What is your grade percentage? ");
+//         string grade = Console.ReadLine();
+//         int percentage = int.Parse(grade);
+// {
+//     if (percentage >= 90 )
+//         Console.WriteLine("Your letter grade is: A"); 
+//     else if (percentage >= 80)
+//         Console.WriteLine("Your letter grade is: B");
+//     else if (percentage >= 70)
+//        Console.WriteLine("Your letter grade is: C");
+//     else if (percentage >= 60)
+//         Console.WriteLine("Your letter grade is: D");
+//     if (percentage < 60)
+//         Console.WriteLine("Your letter grade is: F");
+//     if (percentage >= 60)
+//         Console.WriteLine("Congratualtions! You passed! You are awesome!");
+//     else
+//         Console.WriteLine("I am so sorry. :( Please try again! I know you can do this! I am so proud of you no matter what!");   
+//     }
+// }
+// }
+
+
+
+// !!!!!!!!!!!!!!!!!!!!!! CORE REQUIREMENTS #3 !!!!!!!!!!!!!!!!!
+
+using System;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int percentage = int.Parse(grade);
+{
+    if (percentage >= 90 )
+    grade = "A";
+    else if (percentage >= 80)
+    grade = "B";
+    else if (percentage >= 70)
+    grade = "C";
+    else if (percentage >= 60)
+    grade = "D";
+    if (percentage < 60)
+    grade = "F";
+
+
+    Console.WriteLine($"Your letter grade is: {grade}"); 
+
+
+    if (percentage >= 60)
+        Console.WriteLine("Congratulations! You passed! You are awesome!");
+    else
+        Console.WriteLine("I am so sorry. :( Please try again! I know you can do this! I am so proud of you no matter what!");   
     }
+}
 }
