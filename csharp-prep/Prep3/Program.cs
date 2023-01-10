471c8e49a7e969f3ec9b75feb832993be0431a97
shaeloco <u360134758@byui.edu>
2023-01-10 02:40:34 -0500

    Req #1 Finished.Fixed Single Line Print Statements

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number? ");
+        string input = Console.ReadLine();
+        int magic = int.Parse(input);
+        Console.WriteLine("What is your guess? ");
+        string guess = Console.ReadLine();
+        int inputGuess = int.Parse(guess);
+
+{
+    if (inputGuess < magic)
+    Console.WriteLine("Higher");
+    if (inputGuess > magic)
+    Console.WriteLine("Lower");
+    if (inputGuess == magic)
+    Console.WriteLine ("You guessed it!!");
+}
     }
 }
