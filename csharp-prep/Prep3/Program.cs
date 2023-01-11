ef294f80b5822fd16f4a70e4afdd6163eaa4d060
shaeloco <u360134758@byui.edu>
2023-01-11 17:25:39 -0500

    Practicing Prepwork.Broken codes

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,19 +5,35 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is the magic number? ");
+
         string input = Console.ReadLine();
-        int magic = int.Parse(input);
-        Console.Write("What is your guess? ");
         string guess = Console.ReadLine();
+        int magic = int.Parse(input);
         int inputGuess = int.Parse(guess);
 
-{
-    if (inputGuess < magic)
-    Console.WriteLine("Higher");
-    if (inputGuess > magic)
-    Console.WriteLine("Lower");
-    if (inputGuess == magic)
-    Console.WriteLine ("You guessed it!!");
-}
-    }
-}
+        
+    do
+    {          
+            if (inputGuess < magic)
+            Console.WriteLine("Higher");
+            if (inputGuess > magic)
+            Console.WriteLine("Lower");
+            if (inputGuess == magic)
+            Console.WriteLine ("You guessed it!!");
+        
+        Console.Write("What is your guess? ");                
+        string guess2 = Console.ReadLine();  
+    } 
+        while (inputGuess != magic);
+
+
+        
+  
+            // guess = Console.ReadLine();
+
+  
+        }       
+
+      
+            
+}       
