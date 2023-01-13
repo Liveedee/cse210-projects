2134fd7aff1ed7fb9491a02a30457ce2b3a5fb70
shaeloco <u360134758@byui.edu>
2023-01-12 22:10:01 -0500

    Finished Prep#4 Req#2

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,24 +5,39 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        
         string add = Console.ReadLine();
         int number = int.Parse(add);
         List<int> numbers = new List<int>();
         int sum = number + number;
-        
+        int runningTotal = 0;
+
         do
         {
-            
-            numbers.Add(number); 
+            numbers.Add(number);
             add = Console.ReadLine();
             sum = int.Parse(add);
-      
-            // Console.WriteLine(numbers.Count);
-       
+            runningTotal += sum;
+           
+            if (sum == 0)
+            {
+                Console.Write($"The Sum is: {number + runningTotal}");
+            }
             
         }
-        while (number != 0);
-            Console.WriteLine($"The Sum is: {sum + number}");
-        
+        while (sum != 0);
+           
+            int averageCounter = (numbers.Count);
+            float average = number + runningTotal;
+            Console.Write($"The average is: {average / averageCounter }");
+            
+            if (number > sum)
+            {
+       
+                Console.WriteLine($"The largest number is: {largest}");
+            }
+
+    
+
     }
 }
