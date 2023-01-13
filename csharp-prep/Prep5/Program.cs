4cf65134c56224b4ee5f36ed64228f888c11d46c
shaeloco <u360134758@byui.edu>
2023-01-13 01:03:01 -0500

    Started on Prep#5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,10 +5,27 @@ class Program
     static void Main(string[] args)
     {
         DisplayWelcome();
+        PromptUserName();
+        PromptUserNumber();            
+        
+        
     }
         static void DisplayWelcome()
         {
             Console.WriteLine("Welcome to the program!");
-        
         }
+        static void PromptUserName()
+        {
+            Console.WriteLine("Please enter your name: ");
+            Console.ReadLine();
+        }
+        static void PromptUserNumber()
+        {
+            Console.WriteLine("Please enter your favorite number: ");
+            Console.ReadLine();
+        }
+        // static void SquareNumber()
+        // {
+
+        // }
     }
