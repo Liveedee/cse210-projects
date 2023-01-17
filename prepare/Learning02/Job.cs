10b9b8f20e0699543fc02fd5a356b15d4aff5077
shaeloco <u360134758@byui.edu>
2023-01-17 17:14:55 -0500

    Updated #3/#4

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,12 +1,12 @@
 public class Job
+
 {
 
-    public string _company = "";
-    public string _jobTitle = "";
-    public int _startYear= 0;
-    public int _endYear = 0;
-
-    public Job()
-    {
-    }
+    Job job1 = new Job();
+    Job._company = "Microsoft";
+    Job._jobTitle = "Software Engineer";
+    Job._startYear = 1990;
+    Job._endYear = 2002
+    Job.SeeAll()
+    
 }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,33 +1,21 @@
 using System;
 
-class Program
+
+public class Program
 {
+
     static void Main(string[] args)
-    {
-        Job1 job1 = new Job1();
-        job1._company = "Microsoft";
-        job1._jobTitle = "Software Engineer";
-        job1._startYear = 1990;
-        job1._endYear = 2002;
-        job1.SeeAll();
-
-        
-    
-    }
-    public class Job1
-{
-
+    {}
     public string _company = "";
     public string _jobTitle = "";
-    public int _startYear= 0;
+    public int _startYear = 0;
     public int _endYear = 0;
 
-    public Job1()
-    {
-    }
+
     public void SeeAll()
     {
         Console.WriteLine($"{_company}");
     }
-}
+    
+
     }
