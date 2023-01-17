5f683a745d7eb8b75a664bba28cd98e7ebda821b
shaeloco <u360134758@byui.edu>
2023-01-16 23:21:56 -0500

    Step #3/#4 Ab Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job1 job1 = new Job1();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 1990;
+        job1._endYear = 2002;
+        job1.SeeAll();
+
+        
+    
+    }
+    public class Job1
+{
+
+    public string _company = "";
+    public string _jobTitle = "";
+    public int _startYear= 0;
+    public int _endYear = 0;
+
+    public Job1()
+    {
+    }
+    public void SeeAll()
+    {
+        Console.WriteLine($"{_company}");
     }
 }
+    }
