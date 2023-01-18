42280526d28aff82b383bc17e0e939d491eafc89
shaeloco <u360134758@byui.edu>
2023-01-17 20:01:03 -0500

    Step#3/#4 done

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -6,10 +6,18 @@ static void Main(string[] args)
     Job job1 = new Job();
     job1._company = "Microsoft";
     job1._jobTitle = "Software Engineer";
-    job1._startYear = 1990;
-    job1._endYear = 2002;
+    job1._startYear = 2019;
+    job1._endYear = 2022;
     job1.SeeAll();
     
+
+    Job job2 = new Job();
+    job2._company = "Apple";
+    job2._jobTitle = "Manager";
+    job2._startYear = 2022;
+    job2._endYear = 2023;
+    job2.SeeAll();
+
 }
 }
 
