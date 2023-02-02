16f8d99716f7fcb01093f5716d20f1bc73623645
shaeloco <u360134758@byui.edu>
2023-02-01 22:00:39 -0500

    Added parameters.Complete

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -9,16 +9,16 @@ class Program
        Console.WriteLine(new1.NewDecimal());
 
 
-       Fraction new2 = new Fraction();
+       Fraction new2 = new Fraction(5);
        Console.WriteLine(new2.NewFraction());
        Console.WriteLine(new2.NewDecimal());
        
 
-       Fraction new3 = new Fraction();
+       Fraction new3 = new Fraction(3, 4);
        Console.WriteLine(new3.NewFraction());
        Console.WriteLine(new3.NewDecimal());
 
-       Fraction new4 = new Fraction();
+       Fraction new4 = new Fraction(1, 3);
        Console.WriteLine(new4.NewFraction());
        Console.WriteLine(new4.NewDecimal());
 
