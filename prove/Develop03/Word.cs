bc4fe58a14ea24770441be8a5c67a35fa7c163a5
shaeloco <u360134758@byui.edu>
2023-02-07 23:30:42 -0500

    Not Correct/but code is running.

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,32 +1,37 @@
 // HIDETHEWORD
+using System;
+using System.Collections.Generic;
 public class Word
 {
     
-    private string _user = "Press enter to continue or type 'quit' to end the program.";
+    private string user = "Press enter to continue or type 'quit' to end the program.";
+    private string _scriptureString = "Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.";
 
-    private string _scripture = "Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.";
-    
-    private string _thankUser = "Thank you for using the Scripture Memorizer!";
+
+
+
+    private string thankUser = "Thank you for using the Scripture Memorizer!";
     public Word()
     {        
     }
 
     public void ShowUser()
     {
-        Console.WriteLine($"\n{_user}");
+        Console.WriteLine($"\n{user}");
         string userChoice = Console.ReadLine();
         
         if (userChoice != "quit")
         {
-        int i = 0;
-        Console.WriteLine($"{_scripture.Replace(" ","_")}");
-        i++;
+            int i = 0;
+            foreach (char word in _scriptureString);
+            Console.WriteLine($"{_scriptureString.Replace($"{_scriptureString[i]}","_")}");
+            i++;
         
     
 
         if (userChoice == "quit")
     
-        Console.WriteLine($"{_thankUser}");
+        Console.WriteLine($"{thankUser}");
     }
 }
 }
