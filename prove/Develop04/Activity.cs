30730cae4929677def800c59e1a200c725fe6d4d
shaeloco <u360134758@byui.edu>
2023-02-21 05:17:57 -0500

    Created menu/Added Breathing Class

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -2,7 +2,9 @@ using System;
 
     public class Activity
     {
-        private string _welcomeMessage = "Hello, Welcome to the mindfulness program";
+        public List<string> _Activities = new List<string>();
+        private string _welcomeMessage = "Hello, Welcome to the mindfulness program" +
+        "\nMenu Options:" + "\n1.Start Breathing Activity" + "\n2.Start Reflecting Activity" + "\n3.Start Listening Activity" +"\n4.Quit" + "\nPlease select an option from the menu.";
         private string _endMessage = "Thank you for using the mindfulness program, have a great day!";
 
     
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -6,5 +6,8 @@ class Program
     {
         Activity activity1 = new Activity();
         Console.WriteLine(activity1.DisplayWelcome());
+            string userInput = Console.ReadLine();
+            if (userInput == "1")
+                Console.WriteLine("start breathing activity");
     }
 }
