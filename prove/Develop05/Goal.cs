69d78b410fdedfb8509c23c0485a55f5889a479c
shaeloco <u360134758@byui.edu>
2023-03-07 15:01:07 -0500

    Creating the 1st class

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,43 @@
+using System;
+
+public class Goal
+{
+
+    private string _name;
+    private string _description;
+    private int _points;
+
+    public string GetName();
+    {
+        Console.Writeline($"What is the name of your goal?:");
+    }
+
+    // public void SetName();
+    // {
+        
+    // }
+
+    // public string GetDescription();
+    // {
+
+    // }
+
+    // public void SetDescription();
+    // {
+
+    // }
+
+    // public string GetPoints();
+    // {
+
+    // }
+
+    // public void SetPoints();
+    // {
+
+    // }
+
+    // public abstract string DisplayGoal();
+    // {
+
+    // }
