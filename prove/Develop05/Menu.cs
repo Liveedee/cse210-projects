f2d697b2297800bb72024cfa316a504ca462c12e
shaeloco <u360134758@byui.edu>
2023-03-17 23:40:35 -0400

    Working on Assignment

--- prove/Develop05/Menu.cs
+++ prove/Develop05/Menu.cs
@@ -2,7 +2,7 @@ using System;
 
 public class Menu
 {
-    public List<string> _FirstMenu = new List<string>();
+    public List<string> _firstMenu = new List<string>();
 
     
 
@@ -10,17 +10,17 @@ public class Menu
     public Menu()
     {  
         
-        _FirstMenu.Add($"{1}. Create New Goal");
-        _FirstMenu.Add($"{2}. List Goals");
-        _FirstMenu.Add($"{3}. Save Goals");
-        _FirstMenu.Add($"{4}. Load Goals");
-        _FirstMenu.Add($"{5}. Record Goals");
-        _FirstMenu.Add($"{6}. Quit");
+        _firstMenu.Add($"{1}. Create New Goal");
+        _firstMenu.Add($"{2}. List Goals");
+        _firstMenu.Add($"{3}. Save Goals");
+        _firstMenu.Add($"{4}. Load Goals");
+        _firstMenu.Add($"{5}. Record Goals");
+        _firstMenu.Add($"{6}. Quit");
 
 
         int i = 0;
         i++;
-        foreach(string menu in _FirstMenu)
+        foreach(string menu in _firstMenu)
         {
             Console.WriteLine($"{menu}");
 
