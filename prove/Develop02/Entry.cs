1f47f1055239b3d85f6c52776c405c9298da70de
shaeloco <u360134758@byui.edu>
2023-03-18 01:39:14 -0400

    Finishing up the myjournaltxt codes

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -13,19 +13,8 @@ using System;
         _Entries.Add("What was the best part of my day?");
         _Entries.Add("How did I see the hand of the Lord in my life today?");
         _Entries.Add("What was the strongest emotion I felt today?");
-        _Entries.Add("If I had one thing I could do over today, what would it be?");
+        _Entries.Add("If I had one thing I could do over today what would it be?");
             
-    
-    int i = 0;
-    while (choice != 5)
-    {
-        foreach(string entry in _Entries)
-        {
-            Console.WriteLine($"{entry}");
-        }
-        int choice = int.Parse(Console.ReadLine());
-        i++;
-        }
     }
         
         // for (int i = 0; i < _Entries.Count; i++)
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,35 +4,52 @@ public class Program
 {
     static void Main(string[] args)
     {
-
-    Journal journal = new Journal();
     
-    
-    PromptGenerator promptGenerator = new PromptGenerator();
-    Console.WriteLine("What would you like to do?");
-    string choice = Console.ReadLine();
-
-    if (choice == "1")
-    {
-        Random random = new Random();  
-        Entry entry1 = new Entry();
-        int promptList = random.Next(entry1._Entries.Count);
-        Console.WriteLine(entry1._Entries[promptList]);
-        string userEntry = Console.ReadLine();
-      
-
-        string fileName = "myJournal.txt";
-
-        using (StreamWriter outputFile = new StreamWriter(fileName))
+        for (int i = 0; i < 20; i++)
         {
-       
-            outputFile.WriteLine(userEntry);
+            Journal journal = new Journal();
+            PromptGenerator promptGenerator = new PromptGenerator();
+            Console.WriteLine("What would you like to do?");
+            string choice = Console.ReadLine();
+           
+            if (choice == "1")
+            {
+                Random random = new Random();  
+                Entry entry1 = new Entry();
+                int promptList = random.Next(entry1._Entries.Count);
+                Console.WriteLine(entry1._Entries[promptList]);
+                string userEntry = Console.ReadLine();
             
-}
-       
+
+                string fileName = "myJournal.txt";
+
+                using (StreamWriter outputFile = new StreamWriter(fileName))
+                {
+                    outputFile.WriteLine(entry1._Entries[promptList]);
+                    outputFile.WriteLine(userEntry);
+                }
+            }
+            else if (choice == "2")
+            {
+                string filename = "myJournal.txt";
+                string[] lines = System.IO.File.ReadAllLines(filename);
+                Console.WriteLine("Prompt:");
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(",");
+                    string readPrompt = parts[0];
+
+                  
+                    {
+                        Console.WriteLine($"{readPrompt}");
+                    }
+        }
+            }        
+        }
+            
+                
+            }    
+
+
+            }
         
-    }    
-
-
-    }
-    }
