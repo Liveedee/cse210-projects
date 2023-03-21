bd37d32f388f8ad87ba3d244d69a446949f20538
shaeloco <u360134758@byui.edu>
2023-03-21 03:03:16 -0400

    Adding comment instances

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -6,25 +6,36 @@ class Program
     {
         Video video1 = new Video();
         video1._title = "The sun is shining";
-        video1._author = "olivia harris";
+        video1._author = "Kitty Katz";
         video1._length = 0;
-        video1.vi
-
+        video1.Videos.Add(video1);
+        
 
         Video video2 = new Video();
-        video2._title = "";
-        video2._author = "";
+        video2._title = "Oh No, It's Raining!";
+        video2._author = "Joe Finger";
         video2._length = 0;
+        video2.Videos.Add(video2);
+
+
+        Comment comment1video = new Comment();
+        comment1video._comment = "Great Video!";
+        comment1video._commenter = "Chris P. Bacon";
+        comment1video._commentAmount = 0;
 
 
 
-        Comment comment1 = new Comment();
-        comment1._comment = "great video!";
-        comment1._commenter = "Somebody";
-        comment1._commentAmount = 0;
+
+        Comment comment2video = new Comment();
+        comment2video._comment = "Awful Video!";
+        comment2video._comment = "Mopey Longbottoms";
+        comment2video._commentAmount = 0;
 
         video1.DisplayAll();
-        comment1.DisplayCommentInfo();
+        comment1video.DisplayCommentInfo();
+        video2.DisplayAll();
+        comment2video.DisplayCommentInfo();
+
 
 
 
