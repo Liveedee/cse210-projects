5c82cea6e835bc12eb57bcbec3a041508b2da263
shaeloco <u360134758@byui.edu>
2023-03-21 02:14:42 -0400

    Creating the videos and values

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-    
+        Video video1 = new Video();
+        video1._title = "";
+        video1._author = "";
+        video1._length = "";
+
+        Video video2 = new Video();
+        video2._title = "";
+        video2._author = "";
+        video2._length = "";
+
     }
 }
