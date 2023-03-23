f0f33a0cdac49f169fabae812d8815bae65ebb65
shaeloco <u360134758@byui.edu>
2023-03-23 01:20:10 -0400

    Working on classes

---
+++ final/Foundation2/Product.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Product
+{
+    public string _name = "";
+    public string _productId = "";
+    public float _price = 0;
+    public int _quantity = 0;
+
+    public Product(string _name, string _productId,float _price, int _quantity )
+    {
+        this._name = _name;
+        this._productId = _productId;
+        this. _price = _price;
+        this. _quantity = _quantity;
+        
+    }
