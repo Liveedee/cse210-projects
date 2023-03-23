6a7591bb09e46d883660fce250e16dfb9cda1020
Liveedee <103865403+Liveedee@users.noreply.github.com>
2023-03-22 20:35:17 -0400

    Update Program.cs
    Accidentally pushed to the main branch instead of prep 4 redo

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,67 +1,48 @@
-// Redoing Assignment 
-
-
 using System;
-
 
 class Program
 {
     static void Main(string[] args)
-    {   
-        List<int> numbers = new List<int>();
-
+    {
         Console.WriteLine("Enter a list of numbers, type 0 when finished");
-        int entry = -1;
-        while (entry != 0)
-        {   
+        
+        string add = Console.ReadLine();
+        int number = int.Parse(add);
+        List<int> numbers = new List<int>();
+        int sum = number + number;
+        int runningTotal = 0;
+
+        do
+        {
+            numbers.Add(number);
+            add = Console.ReadLine();
+            sum = int.Parse(add);
+            runningTotal += sum;
+        
+        if (sum == 0)
+        {
+            Console.Write($"The Sum is: {number + runningTotal}");
+        }
             
-        
-            string addNumber = Console.ReadLine();
-            entry = int.Parse(addNumber);
-
-            if (entry != 0)
-            {
-                numbers.Add(entry);
-            }
         }
+        while (sum != 0);
+           
+            int averageCounter = (numbers.Count);
+            float average = number + runningTotal;
+            Console.Write($"The average is: {average / averageCounter }");
+        
 
-        int sum = 0;
-        foreach(int number in numbers)
+    int largest = numbers[0];
+
+    foreach(int n in numbers)
+    {
+
+        if (number > largest)
         {
-            sum += number;
+            largest = n;
         }
+    }
+        Console.WriteLine($"The largest number is: {number}");
+    }
         
-        Console.WriteLine($"The sum is {sum}");
-    
-        int smallest = numbers[0];
-        foreach(int number in numbers)
-        {
-            if (number < smallest)
-            {
-                smallest = number;
-            }
-        }
-        
-        Console.WriteLine($"The smallest number is: {smallest}");
-    
-        
-
-             
-
-        int largest = numbers[0];
-
-        foreach(int number in numbers)
-        {
-
-            if (number > largest)
-            {
-                largest = number;
-            }
-        }
-        
-        Console.WriteLine($"The largest number is: {largest}");
-    
-        double average = ((double)sum) / numbers.Count;
-        Console.WriteLine($"The average is:  {average}");
-}
 }
